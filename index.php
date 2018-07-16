1e7d7d22719575e95daaccb3b676e0a2f15a5862
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-07-16 11:01:19 +0200

    index

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
       <header>
         <div id="choix">
           <input type="button" name="ident" value="Se connecter" />
-          <a href="liste_film">Visiter</a>
+          <a href="liste_film.php">Visiter</a>
         </div>
 
         <div id="ident">
@@ -57,16 +57,56 @@
 
       <main>
         <section>
-          <button type="button" name="description">Description</button>
-          <button type="button" name="contact">Contact</button>
+          <div class="bouton">
+            <button type="button" name="description">Description</button>
+            <button type="button" name="contact">Contact</button>
+          </div>
 
-          <p class="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <div class="description">
+            <h1>Metropolis</h1>
+            <p>
+              Le Cinéma METROPOLIS est situé en centre-ville, à 300 mètres de la place Ducale, coeur historique de Charleville-Mézières.<br/>
+              L'arrivée de la "voie rapide", elle aussi à 300 mètres du cinéma, permet un accès direct et facile pour ceux qui viennent par la route.
+            </p>
+            <p>
+              Le parking est gratuit tous les jours de 12h00 à 14h00 et après 18h30 jusqu'au lendemain 9h00.
+            </p>
+            <p>
+              Il est gratuit le dimanche toute la journée.
+            </p>
+            <p>
+              La première heure et demie est gratuite le mercredi, le vendredi après-midi et le samedi.
+            </p>
 
-          <p class="contact">
+            <h2>10 salles pour une capacité de 2.000 fauteuils, de 500 à 100 places</h2>
+            <ul>
+              <li>Projection numérique</li>
+              <li>Son "dolby" digital</li>
+              <li>Ecrans "mur à mur"</li>
+              <li>Salles et hall d'accueil climatisés</li>
+            </ul>
+
+            <h2>Ciné-Café et Confiserie</h2>
+            <p>
+              Le CINÉ-CAFÉ est à votre disposition pour prendre un verre, vous retrouver ou calmer une petite faim, avant ou après la séance de cinéma.<br/>
+              Le comptoir CONFISERIE vous propose boissons, pop-corn, friandises et tout ce qu'il faut pour un bon moment de cinéma.
+            </p>
+
+            <h2>Architecture ET Décoration</h2>
+            <p>
+              Développée sur 5.000 m2, l'architecture du cinéma METROPOLIS associe le verre et l'acier.<br/>
+              La décoration du cinéma METROPOLIS repose principalement sur deux grandes toiles tirées du film éponyme de Ftritz LANG (1928). La passerelle du hall ainsi que le traitement du sol rejoignent les décors du film. Volontairement, les systèmes techniques (gaines de ventilation et chemins électriques) ont été laissés apparents.
+            </p>
+
+            <h2>Galerie Metropolis</h2>
+            <p>
+              Pour parfaire le monde des images animées projetées au cinéma METROPOLIS, nous avons voulu donner sa place à la photographie, sans laquelle le cinéma n'aurait jamais existé. Nous vous proposons, tout au long de l'année, des images sur lesquelles vous pourrez jeter un oeil, ou vous attarder. En toute liberté.
+            </p>
+          </div>
+
+          <div class="contact">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          </div>
         </section>
       </main>
 
