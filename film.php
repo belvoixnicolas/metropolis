efc76cba1857ac6def2ea8da4a0a00b6c03136d8
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-07-18 11:34:13 +0200

    liste film

---
+++ film.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+  <html lang="fr">
+    <head>
+      <title>Metropolis</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="ISO-8859-1" />
+      <meta name="theme-color" content="black">
+      <link rel="icon" type="image/x-icon" href="favicon.ico">
+      <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="resource/img/favicon.ico" /><![endif]-->
+      <link rel="stylesheet" type="text/css" href="css/reset.css" />
+      <link rel="stylesheet" type="text/css" href="css/liste.css" />
+      <link href="https://fonts.googleapis.com/css?family=Marck+Script|Open+Sans" rel="stylesheet">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+  </html>
