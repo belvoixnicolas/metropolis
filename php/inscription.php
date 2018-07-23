c865dc1a8dee244af6ec30b65bd221e679217174
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-07-23 12:26:02 +0200

    t

--- php/inscription.php
+++ php/inscription.php
@@ -1,4 +1,9 @@
 <?php
     include 'connexion_dbh.php';
     include 'function.php';
+
+    if (isset($_POST['genre']), isset($_POST['nom']), isset($_POST['prenom']), isset($_POST['mail_ins_1']), isset($_POST['mail_ins_2']), isset($_POST['psw_ins_1']), isset($_POST['psw_ins_2'])) {
+        $reqinscript = $dbh->prepare('INSERT INTO utilisateur` (`ID`, `photo`, `sexe`, `nom`, `prenom`, `mail`, `mdp`, `admin`) VALUES (NULL, '', '', '', '', '', '', '')');
+        
+    }
 ?>
